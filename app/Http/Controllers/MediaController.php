9b0843ce3553054fb24df613380b09134c8d1398
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-06 20:59:11 +0100

    media controller

---
+++ app/Http/Controllers/MediaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\media;
+use Illuminate\Http\Request;
+
+class MediaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(media $create_media_table)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, media $create_media_table)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(media $create_media_table)
+    {
+        //
+    }
+}
