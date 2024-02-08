1ccf4dc93e398d4a8554902c05ccb3cdcea1f040
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-08 11:23:22 +0100

    update route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/';
+    public const DASHBOARD = '/back';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
