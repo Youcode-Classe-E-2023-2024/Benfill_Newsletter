f1457e02176cf099ae77d1534b8e726978fc9b5e
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-13 21:14:32 +0100

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,63 +3,53 @@
         <h1 class="text-3xl uppercase text-indigo-500">Logo</h1>
     </div>
     <ul class="flex flex-col py-4">
-        <li>
-            <a href="#"
-               class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+        @if($admin->hasRole("admin"))
+            <li>
+                <a id="dashboardButton"
+                   class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
                         class="bx bx-home"></i></span>
-                <span class="text-sm font-medium">Dashboard</span>
-            </a>
-        </li>
+                    <span class="text-sm font-medium">Dashboard</span>
+                </a>
+            </li>
+        @endif
         <li>
-            <a href="#"
-               class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+            <a id="templateButton"
+               class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
-                        class="bx bx-music"></i></span>
-                <span class="text-sm font-medium">Music</span>
+                        class="fa-regular fa-envelope" style="color: #8f8f8f;"></i></span>
+                <span class="text-sm font-medium">Mail Templates</span>
             </a>
         </li>
         <li>
-            <a href="#"
-               class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
-                        class="bx bx-drink"></i></span>
-                <span class="text-sm font-medium">Drink</span>
-            </a>
-        </li>
-        <li>
-            <a href="#"
-               class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
-                        class="bx bx-shopping-bag"></i></span>
-                <span class="text-sm font-medium">Shopping</span>
-            </a>
-        </li>
-        <li>
-            <a href="#"
-               class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
-                        class="bx bx-chat"></i></span>
-                <span class="text-sm font-medium">Chat</span>
-            </a>
-        </li>
-        <li>
-            <a href="#"
-               class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+            <a id="membersButton"
+               class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
                         class="bx bx-user"></i></span>
-                <span class="text-sm font-medium">Profile</span>
+                <span class="text-sm font-medium">Members</span>
+                <span class="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">{{count($members)}}</span>
             </a>
         </li>
         <li>
-            <a href="#"
-               class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+            <a id="mediaButton"
+               class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
-                        class="bx bx-bell"></i></span>
-                <span class="text-sm font-medium">Notifications</span>
+                        class="fa-solid fa-photo-film" style="color: #8f8f8f;"></i></span>
+                <span class="text-sm font-medium">Media</span>
                 <span class="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
             </a>
         </li>
+
+        @if($admin->hasRole("admin"))
+            <li>
+                <a id="roleButton"
+                   class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                    <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
+                            class="fa-solid fa-person-circle-exclamation" style="color: #8f8f8f;"></i></span>
+                    <span class="text-sm font-medium">Role Management</span>
+                </a>
+            </li>
+        @endif
         <li>
             <form action="/logout" method="post">
                 @csrf
