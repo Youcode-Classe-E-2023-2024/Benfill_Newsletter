2c7afb9747d643e6e1c90c7a99930ce58cf14c4f
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-13 21:13:42 +0100

    Create AdminSeeder.php

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $firstUser = User::first();
+
+        $firstUser->assignRole('admin');
+    }
+}
