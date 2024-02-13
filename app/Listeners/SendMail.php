4ed95ff00199833c0a0ae77e6daf1a3709620049
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-13 21:11:53 +0100

    Create SendMail.php

---
+++ app/Listeners/SendMail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\Subscribed;
+use App\Mail\subscribedMessage;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class SendMail
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Subscribed $event)
+    {
+        $test = Mail::to($event->email)->send(new subscribedMessage($event->email));
+    }
+}
