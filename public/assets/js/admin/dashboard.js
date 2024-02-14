7b629a26964eaff2455d2bdcb5d8ba12a48d1894
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-15 00:21:45 +0100

    Update dashboard.js

--- public/assets/js/admin/dashboard.js
+++ public/assets/js/admin/dashboard.js
@@ -23,6 +23,12 @@ let roleButton = document.getElementById('roleButton');
 let membersSection = document.getElementById('membersSection');
 let membersButton = document.getElementById('membersButton');
 
+// Media list and card
+let listSection = document.getElementById('listSection');
+let listButton = document.getElementById('listButton');
+let cardSection = document.getElementById('cardSection');
+let cardButton = document.getElementById('cardButton');
+
 $(mediaButton).on('click', () => {
     mediaSection.classList.remove("hidden");
     analyticsSection.classList.add("hidden");
@@ -112,5 +118,16 @@ $(membersButton).on('click', () => {
     roleSection.classList.add("hidden");
     templateForm.classList.add("hidden");
     mediaFormSection.classList.add("hidden");
+
+})
+
+$(cardButton).on('click', () => {
+    cardSection.classList.remove("hidden");
+    listSection.classList.add("hidden");
+
+})
+$(listButton).on('click', () => {
+    listSection.classList.remove("hidden");
+    cardSection.classList.add("hidden");
 
 })
