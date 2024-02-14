56b0bb20f2b3f3ad6dbd567feb6c566c4eee44e7
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-15 00:21:05 +0100

    Create helpers.php

---
+++ app/helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+
+if (!function_exists('getPicUrl')) {
+    function getPicUrl($id, $fileName)
+    {
+        $picPath = "storage/$id/$fileName";
+        return asset($picPath);
+    }
+}
+
+
+if (!function_exists('getRole')) {
+    function getRole($userId) {
+        $user = User::find($userId);
+        if (!$user) {
+            // User not found
+            return "unknown";
+        }
+
+        if ($user->hasRole("admin")) {
+            $role = "admin";
+        } else {
+            $role = "editor";
+        }
+
+        return $role;
+    }
+}
+
