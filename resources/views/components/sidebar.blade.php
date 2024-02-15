4450c19c9c93396a76f7ba22ab0cb123ece34d3c
Anass Benfillous <anasse.benfillous@gmail.com>
2024-02-15 16:51:32 +0100

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,18 +1,18 @@
 <div class="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden">
     <div class="flex items-center justify-center h-20 shadow-md">
-        <h1 class="text-3xl uppercase text-indigo-500">Logo</h1>
+        <h1 class="text-3xl uppercase text-indigo-500">Newsletter</h1>
     </div>
     <ul class="flex flex-col py-4">
-        @if($admin->hasRole("admin"))
-            <li>
-                <a id="dashboardButton"
-                   class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+        @role("admin")
+        <li>
+            <a id="dashboardButton"
+               class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
                         class="bx bx-home"></i></span>
-                    <span class="text-sm font-medium">Dashboard</span>
-                </a>
-            </li>
-        @endif
+                <span class="text-sm font-medium">Dashboard</span>
+            </a>
+        </li>
+        @endrole
         <li>
             <a id="templateButton"
                class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
@@ -21,35 +21,39 @@
                 <span class="text-sm font-medium">Mail Templates</span>
             </a>
         </li>
+        @role("editor")
         <li>
             <a id="membersButton"
                class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
                         class="bx bx-user"></i></span>
                 <span class="text-sm font-medium">Members</span>
-                <span class="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">{{count($members)}}</span>
+                <span
+                    class="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">{{count($members)}}</span>
             </a>
         </li>
+        @endrole
         <li>
             <a id="mediaButton"
                class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
                         class="fa-solid fa-photo-film" style="color: #8f8f8f;"></i></span>
                 <span class="text-sm font-medium">Media</span>
-                <span class="ml-auto mr-6 text-sm bg-blue-100 rounded-full px-3 py-px text-blue-500">{{count($medias)}}</span>
+                <span
+                    class="ml-auto mr-6 text-sm bg-blue-100 rounded-full px-3 py-px text-blue-500">{{count($medias)}}</span>
             </a>
         </li>
 
-        @if($admin->hasRole("admin"))
-            <li>
-                <a id="roleButton"
-                   class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+        @role("admin")
+        <li>
+            <a id="roleButton"
+               class="cursor-pointer flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                     <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
                             class="fa-solid fa-person-circle-exclamation" style="color: #8f8f8f;"></i></span>
-                    <span class="text-sm font-medium">Role Management</span>
-                </a>
-            </li>
-        @endif
+                <span class="text-sm font-medium">Role Management</span>
+            </a>
+        </li>
+        @endrole
         <li>
             <form action="/logout" method="post">
                 @csrf
